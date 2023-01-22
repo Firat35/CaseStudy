b085909f893167fc498864d2e8ffa79416c2a9a2
frat <firat.palta@gunkom.com.tr>
2023-01-22 20:21:25 +0300

    Add project files.

---
+++ JSONParse/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+var incoming = new List<Item>();
+using (StreamReader r = new StreamReader("response.json"))
+{
+    string json = r.ReadToEnd();
+    incoming = JsonSerializer.Deserialize<List<Item>>(json);
+}
+Console.WriteLine(incoming[0].description);
+Console.ReadLine();
+
+public record struct Item(
+    string description
+);
