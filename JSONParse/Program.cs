4910bc1d473f9aaaf2aa2022e95206f8a78b3983
frat <firat.palta@gunkom.com.tr>
2023-01-23 03:42:55 +0300

    update

--- JSONParse/Program.cs
+++ JSONParse/Program.cs
@@ -1,19 +1,56 @@
-﻿using System.Text.Json;
+﻿using System.ComponentModel;
+using System.Text.Json;
 var incoming = new List<Item>();
 using (StreamReader r = new StreamReader("response.json"))
 {
     string json = r.ReadToEnd();
     incoming = JsonSerializer.Deserialize<List<Item>>(json);
 }
-var i = 1;
-foreach (var item in incoming[0].description.Split("\n", StringSplitOptions.RemoveEmptyEntries))
+
+int n = incoming?.Count() ?? 0;
+var orderedList = incoming.OrderBy(x => x.boundingPoly.vertices[0].y).ToList();
+for (int i = 2; i < n; i += 1)
 {
-    Console.WriteLine("{0,-3}| {1} ", i.ToString(), item);
-    i++;
+    if (Math.Abs(orderedList[i].boundingPoly.vertices[0].y - orderedList[i - 1].boundingPoly.vertices[0].y) < 10)
+    {
+        if(orderedList[i].boundingPoly.vertices[0].y < orderedList[i - 1].boundingPoly.vertices[0].y)
+            orderedList[i-1].boundingPoly.vertices[ 0].y = orderedList[i].boundingPoly.vertices[0].y;
+        else
+            orderedList[i].boundingPoly.vertices[0].y = orderedList[i - 1].boundingPoly.vertices[0].y;
+    }
 }
+var orderedIncoming = orderedList.OrderBy(x => x.boundingPoly.vertices[0].y)
+    .ThenBy(x => x.boundingPoly.vertices[0].x).ToList();
+int count = 1;
+
+string text = String.Format("{0,-3}| ", count.ToString()) + orderedIncoming[1].description + " ";
+
+for (int i = 2; i< n; i+=1)
+{
+    if (orderedIncoming[i].boundingPoly.vertices[0].y == orderedIncoming[i-1].boundingPoly.vertices[0].y)
+    {
+        text += orderedIncoming[i].description + " ";
+    } 
+    else
+    {
+        count++;
+        text += "\n" + String.Format("{0,-3}| ", count.ToString())+ orderedIncoming[i].description + " ";
+    }
+}
+Console.WriteLine(text);
 
 Console.ReadLine();
 
-public record struct Item(
-    string description
-);
+public class Item
+{
+    public string description { get; set; }
+    public BoundingPoly boundingPoly { get; set; }
+}
+
+public class BoundingPoly { 
+    public List<Coord> vertices { get; set; }
+ }
+public class Coord {
+    public int x { get; set; }
+    public int y { get; set; }
+}
